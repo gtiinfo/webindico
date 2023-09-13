f67941a3b49bf3b3a7faf4afa452524144f94ed9
paulo-maculuve <paulomaculuve758@gmail.com>
2023-09-13 22:52:31 +0200

    adicionar controller para email

---
+++ app/Mail/EmailMediadores.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EmailMediadores extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        return $this->subject('Candicatura de Parceiros')
+            ->view('emails.parceiros')
+            ->with([
+                'data' => $this->data,
+            ]);
+    }
+}
